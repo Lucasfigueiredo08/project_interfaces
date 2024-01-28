18a1d39b9b7de4f287ec3230e946f9455a8de52a
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-01-27 22:40:54 -0300

    Creation of the taxservices, braziltaxservices class, adjusting invoice class and adjusting the program class

--- src/main/java/org/nelioalves/application/Program.java
+++ src/main/java/org/nelioalves/application/Program.java
@@ -2,6 +2,8 @@ package org.nelioalves.application;
 
 import org.nelioalves.model.entities.CarRental;
 import org.nelioalves.model.entities.Vehicle;
+import org.nelioalves.model.services.BrazilTaxService;
+import org.nelioalves.model.services.RentalService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,12 +22,28 @@ public class Program {
         System.out.println("Entre com os dados do alguel");
         System.out.print("Modelo do carro: ");
         String carModel = sc.nextLine();
-        System.out.println("Retirada (dd/MM/yyyy HH:mm): ");
+        System.out.print("Retirada (dd/MM/yyyy HH:mm): ");
         LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-        System.out.println("Retorno (dd/MM/yyyy HH:mm): ");
+        System.out.print("Retorno (dd/MM/yyyy HH:mm): ");
         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
         CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.print("Entre com o preço da hora: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Entre com o preço do dia: ");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("FATURA:");
+        System.out.println("Pagamento básico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+
+        sc.close();
 
     }
 }
